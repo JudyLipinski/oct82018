70132844f0d1fe364b7a1c3b220d057d86a54489
Judy Lipinki <judy@karmoxie.com>
2018-10-08 12:52:32 -0400

    demo trad app

---
+++ test.js
@@ -0,0 +1,37 @@
+// // var arr = [
+// //     { firstName: "Bob", age: 34, isRetired: true ,email: "bbb@gmail.com"},
+// //     { firstName: "Mary", age:37, email:"msry@yahoo.com"},
+// //     { firstName: "Jill", email: "jjj@msn.com"}
+// // ];
+
+// // // var secondAge = arr[1].age;
+// // // console.log("second age is",secondAge);
+
+// // for (let index = 0; index < arr.length; index++) {
+// //     const element = arr[index].email;
+// //     console.log(element);
+// // }
+
+// let numbers = [24,12,10,5,100,64,11];
+// numbers.sort();
+
+// console.log(numbers);
+
+// numbers.sort(  (a,b) => {
+//         return a-b;
+// });
+
+// let evens = numbers.filter(
+//     (number) => number % 2 == 0
+// );
+
+// console.log(evens);
+
+// //function addDate() {
+//     document.getElementById("dateInfo").innerText = new Date();
+// //}
+
+setInterval( ()=>{console.log('Hello!')},
+                2000 );
+
+//READ MDN array.prototype
